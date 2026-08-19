7c21095554e8c09c39412de8e94788b330d30fe3
jred1 <cascinojared@gmail.com>
2026-08-19 16:19:48 -0400

    move results panels

--- new site/src/components/results/ResultsTab.tsx
+++ new site/src/components/results/ResultsTab.tsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { DpsPanel } from './DpsPanel';
 import { DmgOverTimeChart } from './DmgOverTimeChart';
-import { SubstatWorthChart } from './SubstatWorthChart';
 import { TeamContributionPanel } from './TeamContributionPanel';
+import { SubstatWorthChart } from './SubstatWorthChart';
 
 export const ResultsTab: React.FC = () => (
   <div className="results-tab-grid">
     <DpsPanel />
     <DmgOverTimeChart />
-    <SubstatWorthChart />
     <TeamContributionPanel />
+    <SubstatWorthChart />
   </div>
 );
